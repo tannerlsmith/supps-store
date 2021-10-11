46265ffb53c8d21a540cbe47ebd0da9bc2d5973c
tannerlsmith <tanner.smith@uvm.edu>
2021-10-11 19:44:19 -0400

    server complete, added models and schemas

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -23,6 +23,69 @@ db.once('open', async () => {
             category: categories[0]._id,
             price: 14.99,
             quantity: 40
+        },
+        {
+            name: 'Vitamin C',
+            description: 
+                '1000 mg of vitamin c',
+            // image: '',
+            category: categories[0]._id,
+            price: 14.99,
+            quantity: 40
+        },
+        {
+            name: 'Magnesium',
+            description: 
+                '300mg of magnesium',
+            // image: '',
+            category: categories[1]._id,
+            price: 17.99,
+            quantity: 40
+        },
+        {
+            name: 'Iodine',
+            description: 
+                '300mg of iodine',
+            // image: '',
+            category: categories[1]._id,
+            price: 17.99,
+            quantity: 40
+        },
+        {
+            name: 'Beef Liver',
+            description: 
+                '300mg of dessicated beef liver',
+            // image: '',
+            category: categories[2]._id,
+            price: 29.99,
+            quantity: 40
         }
-    ])
+    ]);
+
+    console.log('products seeded');
+
+    await User.deleteMany();
+
+    await User.create({
+        firstName: 'Tamela',
+        lastName: 'Smitta',
+        email: 'tsmitta@testmail.com',
+        password: 'password123',
+        orders: [
+            {
+                products: [products[0]._id, products[0]._id, products[1]._id]
+            }
+        ]
+    });
+
+    await User.create({
+        firstName: 'Elijah', 
+        lastName: 'Holbrook',
+        email: 'eholbrook@testmail.com',
+        password: 'password123'
+    });
+
+    console.log('users seeded');
+
+    process.exit();
 })
